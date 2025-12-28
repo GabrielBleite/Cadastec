fe80b11447ea96b26bc7c681df0339c261e2444f
GabrielBleite <gabriel.barbosaleite78@gmail.com>
2025-12-28 07:52:36 -0300

    Fix ES module resolution for three.js controls

---
+++ src/utils/threeLoader.js
@@ -0,0 +1,3 @@
+// Centraliza imports de Three.js e OrbitControls via ES Modules com resolução explícita
+export * as THREE from "https://unpkg.com/three@0.165.0/build/three.module.js?module";
+export { OrbitControls } from "https://unpkg.com/three@0.165.0/examples/jsm/controls/OrbitControls.js?module";
